97008c603b03b871068e68d397d3ddfe95b5552b
Prabha  Narayanappa <prabhadarshan2013@gmail.com>
2023-02-07 19:35:25 +0100

    Added comments

--- MongoDBConsole/MongoDBConsole/Program.cs
+++ MongoDBConsole/MongoDBConsole/Program.cs
@@ -14,12 +14,17 @@ namespace MongoDBConsole
             var collection = database.GetCollection<BsonDocument>("WareHouse");
 
             var document = new BsonDocument { { "ProductName", "Phone" }, { "Description", "Mobile Electronics" }, { "Price", 500 } };
-
+            Console.WriteLine("Inserting new item");
+            Console.WriteLine(document);
+            Console.WriteLine();
+            //*I am inserting data here-This satisfies create in CRUD
             Insert(collection, document);
 
+            //*I am printing to see if the data is inserted - This satisfies read in CRUD
             PrintAll(collection);
 
             var findFilter = Builders<BsonDocument>.Filter.Eq("ProductName", "Phone");
+            //* I am finding the record to update- 
             var findData = FindOne(collection, findFilter);
             foreach (var item in findData)
             {
@@ -29,6 +34,7 @@ namespace MongoDBConsole
             }
 
             var update = Builders<BsonDocument>.Update.Set("Price", 1000);
+            //* I am updating the record found above - This satisfies update in CRUD
             Update(collection, findFilter, update);
             var updatedData = FindOne(collection, findFilter);
             foreach (var item in updatedData)
@@ -40,6 +46,7 @@ namespace MongoDBConsole
 
             Console.WriteLine();
             Console.WriteLine("Deleting Item Phone");
+            //* I am deleting the record here- This satisfies delete in CRUD
             Delete(collection, findFilter);
 
 
